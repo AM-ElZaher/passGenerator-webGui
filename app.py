5d5a7f54fc2fc27569df58c70a07e4408b0000e1
Ahmed Mohsen <53126083+AM-ElZaher@users.noreply.github.com>
2022-03-04 15:45:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,33 @@ def index():
     lowers = string.ascii_letters
     numbs = string.digits
     sym = string.punctuation
-    length = requester = request.form.get('passCount')
+    length = request.form.get('passCount')
+    
     if request.method == 'POST':
         pwdlength = int(length)
-        pwd = (''.join(random.choice(numbs + lowers + sym) for i in range(pwdlength)))
+        spStat = request.form.get('chkSp')
+        nmStat = request.form.get('chkNm')
+        btStat = request.form.get('chkBoth')
+        # pwd = (''.join(random.choice(numbs + lowers + sym) for i in range(pwdlength)))
+        if ((str(spStat)) != "None") and ((str(nmStat)) != "None") and ((str(btStat)) != "None"):
+            print("invalid")
+
+
+
+        if (str(spStat)) != "None":
+            pwd = (''.join(random.choice(lowers + sym) for i in range(pwdlength)))
+            return render_template('index.html', genPass=pwd)
+
+        elif (str(nmStat)) != "None":
+            pwd = (''.join(random.choice(numbs + lowers) for i in range(pwdlength)))
+            return render_template('index.html', genPass=pwd)
+
+        elif ((str(btStat)) != "None"):
+            pwd = (''.join(random.choice(numbs + lowers + sym) for i in range(pwdlength)))
+            return render_template('index.html', genPass=pwd)
+
+        else:
+            pwd = (''.join(random.choice(lowers) for i in range(pwdlength)))
         return render_template('index.html', genPass=pwd)
     else:
         return render_template('index.html')
